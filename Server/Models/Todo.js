dcfe0c0e63355298c6569efbd83a71e14ba3da38
Rishi.R.Vishwakarma <149783805+RishiWebDeveloper1@users.noreply.github.com>
2024-09-04 14:56:03 +0530

    Delete Server directory

--- Server/Models/Todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const TodoSchema = new mongoose.Schema({
-    task: { type: String, required: true }
-});
-
-const TodoModel = mongoose.model("todos", TodoSchema);
-module.exports = TodoModel;
